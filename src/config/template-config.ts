b1afd96b6ad77ceef082d57605838170131ddb9a
FF <>
2020-12-18 19:56:57 +0800

    添加新的template

--- src/config/template-config.ts
+++ src/config/template-config.ts
@@ -2,7 +2,7 @@
  * @Description: 配置文件
  * @Author: Fu Fei
  * @Date: 2020-12-18 17:11:30
- * @LastEditTime: 2020-12-18 18:10:49
+ * @LastEditTime: 2020-12-18 19:51:56
  * @LastEditors: Fu Fei
  * @FilePath: \generate-file\src\config\template-config.ts
  */
@@ -24,6 +24,10 @@ export default {
         normal: getPath("../template/project/normal/"),
     },
     plugins: {
+        init: {
+            name: "init",
+            template: getPath(["../template/config/.browserslistrc"]),
+        },
         gitignore: {
             name: "eslint",
             template: getPath(["../template/config/.gitignore"]),
@@ -34,32 +38,73 @@ export default {
                 "../template/config/.eslintrc.js",
                 "../template/config/.prettierrc",
             ]),
-            devDependencies: {
-                "@types/node": "^14.14.14",
-                prettier: "^2.2.1",
-                "eslint-config-prettier": "^7.0.0",
-                "eslint-plugin-prettier": "^3.3.0",
-                eslint: "^7.15.0",
+            packageConfig: {
+                devDependencies: {
+                    "@types/node": "^14.14.14",
+                    prettier: "^2.2.1",
+                    "eslint-config-prettier": "^7.0.0",
+                    "eslint-plugin-prettier": "^3.3.0",
+                    eslint: "^7.15.0",
+                },
+                dependencies: {},
             },
-            dependencies: {},
         },
         tslint: {
             name: "tslint",
             template: getPath([
+                "../template/config/tsconfig.json",
                 "../template/config/.eslintrc.js",
                 "../template/config/.prettierrc",
             ]),
-            devDependencies: {
-                "@types/node": "^14.14.14",
-                prettier: "^2.2.1",
-                "eslint-config-prettier": "^7.0.0",
-                "eslint-plugin-prettier": "^3.3.0",
-                eslint: "^7.15.0",
-                "@typescript-eslint/eslint-plugin": "^4.10.0",
-                "@typescript-eslint/parser": "^4.10.0",
-                typescript: "^4.1.3",
+            packageConfig: {
+                devDependencies: {
+                    "@types/node": "^14.14.14",
+                    prettier: "^2.2.1",
+                    "eslint-config-prettier": "^7.0.0",
+                    "eslint-plugin-prettier": "^3.3.0",
+                    eslint: "^7.15.0",
+                    "@typescript-eslint/eslint-plugin": "^4.10.0",
+                    "@typescript-eslint/parser": "^4.10.0",
+                    typescript: "^4.1.3",
+                },
+                dependencies: {},
             },
-            dependencies: {},
+        },
+        commitlint: {
+            name: "commitlint",
+            template: getPath([
+                "../template/config/.cz-config.js",
+                "../template/config/commitlint.config.js",
+            ]),
+            packageConfig: {
+                config: {
+                    commitizen: {
+                        path: "cz-customizable",
+                    },
+                },
+                husky: {
+                    hooks: {
+                        "pre-commit": "lint-staged",
+                        "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
+                        "post-commit": "git push origin --all",
+                    },
+                },
+                devDependencies: {
+                    husky: "^3.1.0",
+                    "lint-staged": "^9.5.0",
+                    commitizen: "^4.0.3",
+                    commitlint: "^8.2.0",
+                    "cz-customizable": "^6.2.0",
+                    "@commitlint/cli": "^8.2.0",
+                    "@commitlint/config-angular": "^8.2.0",
+                    "@commitlint/config-conventional": "^8.2.0",
+                },
+                "lint-staged": {
+                    "src/**/*.{js,ts}": [
+                        "eslint --rule \"no-console: ['warn', { allow: ['warn', 'error'] }]\"",
+                    ],
+                },
+            },
         },
     },
 };
--- src/fun-unit/create-package.ts
+++ src/fun-unit/create-package.ts
@@ -2,14 +2,15 @@
  * @Description:创建 package.json
  * @Author: Fu Fei
  * @Date: 2020-12-18 17:14:47
- * @LastEditTime: 2020-12-18 17:51:17
+ * @LastEditTime: 2020-12-18 19:55:17
  * @LastEditors: Fu Fei
  * @FilePath: \generate-file\src\fun-unit\create-package.ts
  */
 import fs from "fs";
 import path from "path";
 import templateConfig from "@/config/template-config";
-const template = {
+import createFile from "@/tools/createFile";
+const packageTemplate = {
     name: "",
     version: "0.0.1",
     description: "根据JSON结构创建目录",
@@ -20,18 +21,31 @@ const template = {
     devDependencies: {},
     dependencies: {},
 };
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function mergeConfig(origin, target): any {
+    if (!target) return origin;
+    for (const key of Object.keys(target)) {
+        const originData = typeof origin[key] == "object" ? origin[key] : {};
+        if (typeof target[key] == "string") {
+            origin[key] = target[key];
+        } else {
+            origin[key] = {
+                ...target[key],
+                ...originData,
+            };
+        }
+    }
+    return origin;
+}
 export default function createPackageJson(
     config: TYPE_CONFIG,
     outDir: string
 ): void {
-    template.name = config.name;
-    template.author = config.author;
+    packageTemplate.name = config.name;
+    packageTemplate.author = config.author;
 
-    const plugins = ["gitignore", ...config.plugins];
-
-    let devDependencies = {};
-    let dependencies = {};
+    const plugins = ["init", "gitignore", "commitlint", ...config.plugins];
+    let result = packageTemplate;
 
     plugins.forEach((name) => {
         const pluginParam = templateConfig.plugins[name];
@@ -49,22 +63,8 @@ export default function createPackageJson(
                 }
             );
         });
-
-        if (pluginParam.devDependencies) {
-            devDependencies = {
-                ...pluginParam.devDependencies,
-                ...devDependencies,
-            };
-        }
-
-        if (pluginParam.dependencies) {
-            dependencies = {
-                ...pluginParam.dependencies,
-                ...dependencies,
-            };
-        }
+        result = mergeConfig(result, pluginParam.packageConfig);
     });
 
-    template.dependencies = dependencies;
-    template.devDependencies = devDependencies;
+    createFile(outDir + "/" + "package.json", JSON.stringify(result));
 }
